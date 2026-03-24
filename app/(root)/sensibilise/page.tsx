85204ea8a090914add68abaf9e94d779fc5ad4a7
Freddymk <20mm313@esisalama.org>
2026-03-24 15:54:05 +0200

    sensibilise page start

---
+++ app/(root)/sensibilise/page.tsx
@@ -0,0 +1,11 @@
+
+
+const FundaSensibilisePage = () => {
+  return (
+    <main className="px-6 md:px-12 py-12 space-y-16">
+      Funda Sensibilise
+    </main>
+  );
+}
+
+export default FundaSensibilisePage
