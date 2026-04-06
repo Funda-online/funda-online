f9cf5f6264b8e132ee1a90a7fad0c8825ff58fbd
Freddymk <20mm313@esisalama.org>
2026-04-06 13:42:09 +0200

    id page sensibilise schema

--- sanity/schemaTypes/sensibilise.ts
+++ sanity/schemaTypes/sensibilise.ts
@@ -1,22 +1,39 @@
-export default {
-  name: 'sensibilisation',
-  title: 'Sensibilisations Passées',
-  type: 'document',
+import { defineType } from "sanity";
+
+export default defineType({
+  name: "sensibilisation",
+  title: "Sensibilisations Passées",
+  type: "document",
   fields: [
-    { name: 'title', title: 'Titre de l\'activité', type: 'string' },
-    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title' } },
-    // AJOUT : Le badge (ex: Éducation)
-    { name: 'category', title: 'Catégorie (Badge)', type: 'string', initialValue: 'Éducation' },
-    { name: 'location', title: 'Lieu', type: 'string' }, // ex: LUBUMBASHI [cite: 8]
-    { name: 'date', title: 'Date de l\'activité', type: 'date' }, // ex: 10 FÉVRIER 2026 [cite: 51]
-    { name: 'summary', title: 'Résumé/Introduction', type: 'text' },
-    { name: 'content', title: 'Contenu complet (Résumé)', type: 'array', of: [{type: 'block'}] },
-    { name: 'mainImage', title: 'Image Principale', type: 'image', options: { hotspot: true } },
+    { name: "title", title: "Titre de l'activité", type: "string" },
+    { name: "slug", title: "Slug", type: "slug", options: { source: "title" } },
+
     {
-      name: 'gallery',
-      title: 'Galerie d\'images',
-      type: 'array',
-      of: [{ type: 'image' }]
-    }
-  ]
-}
+      name: "category",
+      title: "Catégorie (Badge)",
+      type: "string",
+      initialValue: "Éducation",
+    },
+    { name: "location", title: "Lieu", type: "string" }, 
+    { name: "date", title: "Date de l'activité", type: "date" }, 
+    { name: "summary", title: "Résumé/Introduction", type: "text" },
+    {
+      name: "content",
+      title: "Contenu complet (Résumé)",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "mainImage",
+      title: "Image Principale",
+      type: "image",
+      options: { hotspot: true },
+    },
+    {
+      name: "gallery",
+      title: "Galerie d'images",
+      type: "array",
+      of: [{ type: "image" }],
+    },
+  ],
+});
